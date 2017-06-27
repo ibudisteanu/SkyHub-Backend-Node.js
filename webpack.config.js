606363c924db377603830fbfb56682288463db58
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-06-28 02:57:37 +0300

    .gitignore is now working

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,15 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: 'main.bundle.js'
     },
+    resolve:{
+        alias:{
+            '~application': path.resolve( __dirname + '/application'),
+            '~modules': path.resolve( __dirname + '/application/modules'),
+            '~DB': path.resolve( __dirname , 'application/modules/DB'),
+            '~REST': path.resolve( __dirname + '/application/modules/REST'),
+            '~utils': path.resolve( __dirname + '/application/modules/utils'),
+        },
+    },
     target: 'node', // in order to ignore built-in modules like path, fs, etc.
     externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
     module: {
