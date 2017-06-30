75e678db0c6f56e7c236ba27b2b2da5ebe89e824
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-07-01 00:24:33 +0300

    Searcges Helper ... making the build Search Tables synchronously

--- application/modules/REST/searches/helpers/Searches.helper.ts
+++ application/modules/REST/searches/helpers/Searches.helper.ts
@@ -138,27 +138,52 @@ class Searches {
 
         let parent = this;
 
-        forumModelORM.find(async function (err, ids){
 
-            for (let i=0; i<ids.length; i++)
-                await this.addForumToSearch(null,ids[i], 0);
-        }.bind(this) );
+        let promise = new Promise( (resolve) => {
+            forumModelORM.find(async function (err, ids){
 
-        userModelORM.find(async function (err, ids){
+                for (let i=0; i<ids.length; i++)
+                    await this.addForumToSearch(null,ids[i], 0);
 
-            for (let i=0; i<ids.length; i++)
-                await this.addUserToSearch(null,ids[i], 0);
-        }.bind(this) );
+                resolve(true);
+            }.bind(this) );
+        });
 
-        topicModelORM.find(async function (err, ids){
-            for (let i=0; i<ids.length; i++)
-                await this.addTopicToSearch(null,ids[i], 0);
-        }.bind(this) );
+        await promise;
 
-        replyModelORM.find(async function (err, ids){
-            for (let i=0; i<ids.length; i++)
-                await this.addReplyToSearch(null,ids[i], 0);
-        }.bind(this) );
+        promise = new Promise( (resolve) => {
+            userModelORM.find(async function (err, ids) {
+
+                for (let i = 0; i < ids.length; i++)
+                    await this.addUserToSearch(null, ids[i], 0);
+
+                resolve(true);
+            }.bind(this));
+        });
+
+        await promise;
+
+        promise = new Promise( (resolve) => {
+            topicModelORM.find(async function (err, ids) {
+                for (let i = 0; i < ids.length; i++)
+                    await this.addTopicToSearch(null, ids[i], 0);
+
+                resolve(true);
+            }.bind(this));
+        });
+
+        await promise;
+
+        promise = new Promise( (resolve) => {
+            replyModelORM.find(async function (err, ids){
+                for (let i=0; i<ids.length; i++)
+                    await this.addReplyToSearch(null,ids[i], 0);
+
+                resolve(true);
+            }.bind(this) );
+        });
+
+        await promise;
 
     }
 
