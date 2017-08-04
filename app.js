4e7632fb13c781fcbce4e3124b8fd7f7b7eb10a9
Alexandru Ionut Budisteanu <ionutbudisteanu@yahoo.com>
2017-08-04 23:19:27 +0300

    Express public folders bug fix for Unix Systems

--- app.js
+++ app.js
@@ -27,7 +27,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/public", express.static(__dirname + '/public'));
 
 /*//CORS ACCEPTANCE BUG https://enable-cors.org/server_expressjs.html
 app.use(function(req, res, next) {
